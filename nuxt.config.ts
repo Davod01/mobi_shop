5bda40ea1d3a1b21ee7c3757e82b4b8e5191e4d8
unknown <davod.nar@gmail.com>
2023-08-04 11:39:38 +0430

    add nuxt.conf from vuetify branch into master

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,12 @@ export default defineNuxtConfig({
       'gsap'
     ]
   },
+
+  css: [
+    '~/assets/mobi.scss',
+    '~/assets/animation.scss'
+  ],
+
   modules: [
     '@pinia/nuxt',
     // '@nuxtjs/i18n',
@@ -16,7 +22,46 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     'vuetify-nuxt-module'
   ],
+
+  routeRules: {
+    '/': { prerender: true },
+    '/about': { prerender: true }
+  },
+
+  auth: {
+    provider: {
+      type: 'authjs'
+    }
+  },
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+    // pageTransition: { name: 'slide-in-right', mode: 'out-in' }
+  },
+
   typescript: {
     shim: false
+  },
+
+  // i18n: {
+  //   vueI18n: './I18n/i18n.config.ts'
+  // }
+
+  vuetify: {
+    vuetifyOptions: {
+      locale: {
+        locale: 'fa',
+        messages: { fa }
+      }
+    }
+  },
+
+  devtools: {
+    enabled: true
   }
+
+  // plugins: ['/f:/projects/other/mobi_shop/plugins/01.trpcClient.ts']
 })
